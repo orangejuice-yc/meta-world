fb9b6450dacf2c31ab9838f52a8d6b3b18b70f77
orangejuice-yc <1070743214@qq.com>
2023-11-29 23:33:49 +0800

    修改canvas

--- src/pages/World.js
+++ src/pages/World.js
@@ -1,22 +1,42 @@
-import { Suspense } from 'react'
+import { useState } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
 
 import Model from './components/Model'
 import Env from './components/Env'
 import Overlay from './components/Overlay'
 
 export default function World() {
+  const [isAutoRotate, setAutoRotate] = useState(true)
+  const [cameraPosition, setCameraPosition] = useState([100, 100, 100]);
+  const TogoCameraView = () => {
+    console.log(11111)
+    setCameraPosition([10, 10, 10])
+    setAutoRotate(false)
+  }
   return (
-    <div className='world-canvas'>
-      <Canvas style={{width:'100vw',height:'100vh'}} camera={{ position: [100, 100, 100], fov: 25 }}>
-        <Suspense fallback={null}>
-          <Model />
-          <Env />
-        </Suspense>
-        <OrbitControls minPolarAngle={0} maxPolarAngle={Math.PI / 2.1} />
+    <div className='world-container'>
+      <Canvas style={{ width: '100vw', height: '100vh' }} camera={{ position: [500, 500, 500], fov: 55, near: 0.1 }}>
+        <Model />
+        <Env />
+        <OrbitControls
+          autoRotate={isAutoRotate}
+          autoRotateSpeed={2}
+          enablePan={false}
+          enableZoom={false}
+          minPolarAngle={0}
+          maxPolarAngle={Math.PI / 4}
+        />
+        <axesHelper args={[100]} />
+        <PerspectiveCamera
+          makeDefault  // 使摄像头成为默认相机
+          position={cameraPosition}  // 设置摄像头位置
+          fov={75}  // 设置视野范围
+          near={0.1}  // 设置近裁剪面
+          far={1000}  // 设置远裁剪面
+        />
       </Canvas>
-      <Overlay />
+      <Overlay TogoCameraView={TogoCameraView} />
     </div>
   )
 }
--- src/pages/components/Overlay.js
+++ src/pages/components/Overlay.js
@@ -1,14 +1,13 @@
 
-export default function Overlay() {
+export default function Overlay(props) {
+  console.log(props)
   return (
-    <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
-      {/* <a href="https://pmnd.rs/" style={{ position: 'absolute', bottom: 40, left: 90, fontSize: '13px' }}>
-        pmnd.rs
-        <br />
-        dev collective
-      </a> */}
-      <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '28px',fontWeight:'bold' }}>META WORLD</div>
+    <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
+      <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '28px', fontWeight: 'bold' }}>META WORLD</div>
       <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>ZIMA BLUE TEAM</div>
+      <button style={{ position: 'absolute', bottom: 40, left: 40, fontSize: '13px', zIndex: 999, background: 'yellow' }} onClick={props.TogoCameraView}>
+        ZOOM IT
+      </button>
     </div>
   )
 }
